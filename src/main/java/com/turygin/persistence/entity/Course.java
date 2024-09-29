5ade615596498c23090ec492940e613173439fea
Dmitrii Turygin <dmitrii.turygin@gmail.com>
2024-09-28 21:34:50 -0500

    Added Course DTO and tests

---
+++ src/main/java/com/turygin/persistence/entity/Course.java
@@ -0,0 +1,253 @@
+package com.turygin.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+/**
+ * A class representing a course.
+ */
+@Entity
+@Table(name = "course")
+public class Course {
+
+    /** Unique course ID. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    /** Course title. */
+    @Column(name = "title")
+    private String title;
+
+    /** Course description. */
+    @Column(name = "description")
+    private String description;
+
+    /** Number of credits. */
+    @Column(name = "credits")
+    private byte credits;
+
+    /** Course number. */
+    @Column(name = "number")
+    private short number;
+
+    /** Course department. */
+    @ManyToOne
+    @JoinColumn(name = "department_id",
+            foreignKey = @ForeignKey(name = "course_department")
+    )
+    private Department department;
+
+    /**
+     * Empty constructor.
+     */
+    public Course() {}
+
+    /**
+     * Instantiates a course object.
+     *
+     * @param title       course title
+     * @param description course description
+     * @param credits     number of credit hours
+     * @param number      course number
+     * @param department  department object
+     */
+    public Course(String title, String description, int credits, int number, Department department) {
+        this.title = title;
+        this.description = description;
+        this.department = department;
+        setCredits(credits);
+        setNumber(number);
+    }
+
+    /**
+     * Instantiates a course object omitting the department.
+     *
+     * @param title       course title
+     * @param description course description
+     * @param credits     number of credit hours
+     * @param number      course number
+     */
+    public Course(String title, String description, int credits, int number) {
+        this.title = title;
+        this.description = description;
+        setCredits(credits);
+        setNumber(number);
+    }
+
+    /**
+     * Gets credits.
+     * @return the credits
+     */
+    public int getCredits() {
+        return Byte.toUnsignedInt(credits);
+    }
+
+    /**
+     * Sets credits.
+     * @param credits the credits
+     */
+    public void setCredits(byte credits) {
+        this.credits = credits;
+    }
+
+    /**
+     * Sets credits, safely casting it from an integer.
+     * @param credits the credits as an integer
+     * @throws IllegalArgumentException if the number of credits is non-positive or does not fit into an unsigned byte
+     */
+    public void setCredits(int credits) {
+        if (credits <= 0) throw new IllegalArgumentException("The number of credits must be positive.");
+
+        if (credits > Byte.MAX_VALUE + Math.abs(Byte.MIN_VALUE)) {
+            throw new IllegalArgumentException("The number of credits must fit into an unsigned byte.");
+        }
+        setCredits((byte) credits);
+    }
+
+    /**
+     * Gets department.
+     * @return the department
+     */
+    public Department getDepartment() {
+        return department;
+    }
+
+    /**
+     * Sets department.
+     * @param department the department
+     */
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    /**
+     * Gets description.
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets description.
+     * @param description the description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets id.
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     * @param id the id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets course number.
+     * @return the course number
+     */
+    public int getNumber() {
+        return Short.toUnsignedInt(number);
+    }
+
+    /**
+     * Sets course number.
+     * @param number the course number
+     */
+    public void setNumber(short number) {
+        this.number = number;
+    }
+
+    /**
+     * Sets course number, safely casting it from an integer.
+     * @param number the course number as an integer
+     * @throws IllegalArgumentException if the course number is non-positive or does not fit into an unsigned short
+     */
+    public void setNumber(int number) {
+        if (number <= 0) throw new IllegalArgumentException("The course number must be positive.");
+
+        if (number > Short.MAX_VALUE + Math.abs(Short.MIN_VALUE)) {
+            throw new IllegalArgumentException("The number of credits must fit into an unsigned short.");
+        }
+        setNumber((short) number);
+    }
+
+    /**
+     * Gets title.
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets title.
+     * @param title the title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets the course code.
+     * @return course code
+     */
+    public String getCode() {
+        return String.format("%s %d", department.getCode(), number);
+    }
+
+    /**
+     * Generates a string representation of the course object.
+     * @return string representation of the course
+     */
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Course{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", credits=").append(getCredits());
+        sb.append(", number=").append(getNumber());
+        sb.append(", department=").append(department.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+
+    /**
+     * Performs deep equality comparison. Ignores department field.
+     * @param o object to compare to
+     * @return true if all fields of the object are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id &&
+                credits == course.credits &&
+                number == course.number &&
+                Objects.equals(title, course.title) &&
+                Objects.equals(description, course.description);
+    }
+
+    /**
+     * Generates a hash based on object fields. Ignores department field.
+     * @return hash value
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, credits, number);
+    }
+}
---
+++ src/main/test/com/turygin/persistence/entity/CourseTest.java
@@ -0,0 +1,102 @@
+package com.turygin.persistence.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    private Course course1;
+    private Course course2;
+
+    @BeforeEach
+    void populateUsers() {
+        Department compSci = new Department("CS", "Computer Science");
+        course1 = new Course("Introduction to Python", "Introduces students to Python.", 3, 101, compSci);
+        course1.setId(1);
+        course2 = new Course("Advanced Java", "Java EE.", 4, 203, compSci);
+        course2.setId(2);
+    }
+
+    @Test
+    void getCode() {
+        assertEquals("CS 101", course1.getCode());
+    }
+
+    @Test
+    void creditsNegative() {
+        int credits = -13;
+        assertThrows(IllegalArgumentException.class, () -> course1.setCredits(credits));
+    }
+
+    @Test
+    void creditsZero() {
+        int credits = 0;
+        assertThrows(IllegalArgumentException.class, () -> course1.setCredits(credits));
+    }
+
+    @Test
+    void creditsMaxByte() {
+        int credits = 255;
+        course1.setCredits(credits);
+        assertEquals(credits, course1.getCredits());
+    }
+
+    @Test
+    void creditsTooLarge() {
+        int credits = 256;
+        assertThrows(IllegalArgumentException.class, () -> course1.setCredits(credits));
+    }
+
+    @Test
+    void numberNegative() {
+        int number = -13;
+        assertThrows(IllegalArgumentException.class, () -> course1.setNumber(number));
+    }
+
+    @Test
+    void numberZero() {
+        int number = 0;
+        assertThrows(IllegalArgumentException.class, () -> course1.setNumber(number));
+    }
+
+    @Test
+    void numberMaxShort() {
+        int number = 65535;
+        course1.setNumber(number);
+        assertEquals(number, course1.getNumber());
+    }
+
+    @Test
+    void numberTooLarge() {
+        int number = 65536;
+        assertThrows(IllegalArgumentException.class, () -> course1.setNumber(number));
+    }
+
+    @Test
+    void testSameObject() {
+        Course course_copy = new Course(course1.getTitle(), course1.getDescription(), course1.getCredits(), course1.getNumber());
+        course_copy.setId(course1.getId());
+
+        assertEquals(course1, course_copy);
+    }
+
+    @Test
+    void testDifferentObject() {
+        assertNotEquals(course1, course2);
+    }
+
+    @Test
+    void testHashCodeSameObject() {
+        Course course_copy = new Course(course1.getTitle(), course1.getDescription(), course1.getCredits(), course1.getNumber());
+        course_copy.setId(course1.getId());
+
+        assertEquals(course1.hashCode(), course_copy.hashCode());
+    }
+
+    @Test
+    void testHashCodeDifferentObject() {
+        assertNotEquals(course1.hashCode(), course2.hashCode());
+    }
+}
