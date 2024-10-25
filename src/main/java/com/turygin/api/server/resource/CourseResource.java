7884c704d6d6ad60643c3eaf8d695f789744b97d
Dmitrii Turygin <dmitrii.turygin@gmail.com>
2024-10-24 22:58:50 -0500

    Fix course search endpoint

--- src/main/java/com/turygin/api/server/resource/CourseResource.java
+++ src/main/java/com/turygin/api/server/resource/CourseResource.java
@@ -1,10 +1,11 @@
 package com.turygin.api.server.resource;
 
 import com.turygin.api.model.CourseBasicDTO;
-import com.turygin.api.model.CourseSearchParams;
 import com.turygin.api.resource.ICourseResource;
 import com.turygin.persistence.dao.CourseDao;
 import com.turygin.persistence.entity.Course;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,10 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * REST API resource.
+ * REST API course resource.
  */
 @Path("/course")
 public class CourseResource implements ICourseResource {
+
+    protected static final Logger LOG = LogManager.getLogger(CourseResource.class);
 
     private final CourseDao COURSE_DAO = new CourseDao();
 
@@ -24,8 +27,12 @@ public class CourseResource implements ICourseResource {
     @Produces({ MediaType.APPLICATION_JSON })
     public CourseBasicDTO getCourse(@PathParam("id") long id) {
         Course course = COURSE_DAO.getById(id);
-        return new CourseBasicDTO(course.getId(), course.getCode(), course.getTitle(),
-                course.getDescription(), course.getCredits());
+        CourseBasicDTO courseDTO = null;
+        if (course != null) {
+            courseDTO = new CourseBasicDTO(course.getId(), course.getCode(), course.getTitle(),
+                    course.getDescription(), course.getCredits());
+        }
+        return courseDTO;
     }
 
     @GET
@@ -41,8 +48,9 @@ public class CourseResource implements ICourseResource {
     @GET
     @Path("/find")
     @Produces({ MediaType.APPLICATION_JSON })
-    public List<CourseBasicDTO> findCourses(@BeanParam CourseSearchParams params) {
-        List<Course> courses = COURSE_DAO.findCourses(params.getTitle(), params.getDepartmentId());
+    public List<CourseBasicDTO> findCourses(@QueryParam("title") String title, @QueryParam("departmentId") long departmentId) {
+        LOG.debug("Received title '{}' and departmentId '{}'", title, departmentId);
+        List<Course> courses = COURSE_DAO.findCourses(title, departmentId);
         List<CourseBasicDTO> courseDTOs = new ArrayList<>();
         courses.forEach(course -> courseDTOs.add(new CourseBasicDTO(course.getId(), course.getCode(), course.getTitle(),
                 course.getDescription(), course.getCredits())));
